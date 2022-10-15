ad01f5a6b4534ae6bd4e42cb8402a7cc1ff6c086
Poytr1 <pooytr1@gmail.com>
2022-10-15 23:05:45 +0800

    fix big number

--- examples/aurory/src/processor.ts
+++ examples/aurory/src/processor.ts
@@ -2,10 +2,10 @@ import { StepStakingProcessor } from './types/solana/step_staking_processor'
 
 StepStakingProcessor.bind({ address: 'StKLLTf7CQ9n5BgXPSDXENovLTCuNc7N2ehvTb6JZ5x' })
   .onStake((args, _, ctx) => {
-    const amount = args.amount.toNumber()
+    const amount = BigInt(args.amount.toString())
     ctx.meter.Counter('total_token').add(amount)
   })
   .onUnstake((args, _, ctx) => {
-    const amount = args.amount.toNumber()
+    const amount = BigInt(args.amount.toString())
     ctx.meter.Counter('total_token').sub(amount)
   })
