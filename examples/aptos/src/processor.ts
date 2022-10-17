c7376107e8514216e80bdd2a9d17adee159b0b52
Fuyao Zhao <zfy0701@gmail.com>
2022-10-17 00:04:14 -0700

    fix(codegen): correctly replace all spaces, fix generic for event type handler

--- examples/aptos/src/processor.ts
+++ examples/aptos/src/processor.ts
@@ -1,22 +1,24 @@
 import { SouffleChefCampaign, CandyMachine } from './types/aptos/souffle'
 
-import { token } from '@sentio/sdk/lib/builtin/aptos/0x3'
+// import { token } from '@sentio/sdk/lib/builtin/aptos/0x3'
 
 SouffleChefCampaign.bind({ startVersion: 6604913 })
   .onEntryPullTokenV2((call, ctx) => {
-    if (!ctx.transaction.success) {
-      return
+    if (!call.arguments_typed) {
+      console.log(JSON.stringify(ctx.transaction))
+      throw Error('processor decode error')
     }
     ctx.meter.Counter('call_num').add(1)
     ctx.meter.Counter('pulled').add(call.arguments_typed[3])
   })
   .onEventBurnEnjoyEvent((evt, ctx) => {
+    if (!evt.data_typed) {
+      console.log(JSON.stringify(evt))
+      throw Error('processor decode error')
+    }
     ctx.meter.Counter('burned').add(1)
   })
   .onTransaction((txn, ctx) => {
-    if (!txn.success) {
-      return
-    }
     if (txn.events) {
       for (const event of txn.events) {
         if (event && event.type === '0x3::token::DepositEvent') {
@@ -27,12 +29,17 @@ SouffleChefCampaign.bind({ startVersion: 6604913 })
   })
 
 CandyMachine.bind({ startVersion: 6604913 }).onEntryPullToken((call, ctx) => {
-  if (!ctx.transaction.success) {
-    return
+  if (!call.arguments_typed) {
+    console.log(JSON.stringify(ctx.transaction))
+    throw Error('processor decode error')
   }
   ctx.meter.Counter('pulled').add(call.arguments_typed[2])
 })
 
-token.bind({ startVersion: 182159141 }).onEventWithdrawEvent((evt: token.WithdrawEventInstance, ctx) => {
-  ctx.meter.Counter('with_draw').add(evt.data_typed.amount, { token: evt.data_typed.id.token_data_id.name })
-})
+// token.bind({ startVersion: 182159141 }).onEventWithdrawEvent((evt: token.WithdrawEventInstance, ctx) => {
+//   if (!evt.data_typed) {
+//     console.log(JSON.stringify(evt))
+//     throw Error("adsfadf")
+//   }
+//   ctx.meter.Counter('with_draw').add(evt.data_typed.amount, { token: evt.data_typed.id.token_data_id.name })
+// })
