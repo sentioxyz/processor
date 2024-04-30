d3097bfd7e58e9923f5538561f9c4dd9a9214908
Yulong <allendragon@gmail.com>
2024-04-30 16:47:43 +0800

    chore(sdk): add fuel asset processor

--- packages/sdk/src/fuel/transaction.ts
+++ packages/sdk/src/fuel/transaction.ts
@@ -19,7 +19,26 @@ export const DEFAULT_FUEL_FETCH_CONFIG: FuelFetchConfig = {
 
 export type FuelTransaction = TransactionSummary
 
-export function decodeFuelTransaction(gqlTransaction: any, abiMap: AbiMap, provider: Provider): FuelTransaction {
+export function decodeFuelTransaction(gqlTransaction: any, provider: Provider): FuelTransaction {
+  const rawPayload = arrayify(gqlTransaction.rawPayload)
+
+  const [decodedTransaction] = new TransactionCoder().decode(rawPayload, 0)
+  const { gasPerByte, gasPriceFactor, maxInputs, gasCosts } = provider.getChain().consensusParameters
+
+  return assembleTransactionSummary({
+    id: gqlTransaction.id,
+    receipts: [],
+    transaction: decodedTransaction,
+    transactionBytes: rawPayload,
+    gqlTransactionStatus: gqlTransaction.status,
+    gasPerByte: bn(gasPerByte),
+    gasPriceFactor: bn(gasPriceFactor),
+    maxInputs,
+    gasCosts
+  })
+}
+
+export function decodeFuelTransactionWithAbi(gqlTransaction: any, abiMap: AbiMap, provider: Provider): FuelTransaction {
   const rawPayload = arrayify(gqlTransaction.rawPayload)
 
   const [decodedTransaction] = new TransactionCoder().decode(rawPayload, 0)
